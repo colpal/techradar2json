084a19663f001dc14fd683da8971034f958b0efc
Nicholas Farley <nicholasafarley@gmail.com>
2025-03-24 14:39:20 -0400

    Run prettier on source files

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { loadTechnologyRadar } from "./lib.ts"
+import { loadTechnologyRadar } from "./lib.ts";
 
 async function main() {
-  console.log(JSON.stringify(await loadTechnologyRadar(process.argv[2])))
+  console.log(JSON.stringify(await loadTechnologyRadar(process.argv[2])));
 }
 
-main()
+main();
