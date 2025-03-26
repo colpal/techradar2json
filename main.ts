80aaf070a16adac041cb4a297b09adf9b4a3f9af
Nicholas Farley <nicholasafarley@gmail.com>
2025-03-26 15:41:40 -0400

    Ensure date outputs do not include (misleading) timestamp

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 import { loadTechnologyRadar } from "./lib.ts";
 
+function jsonReplacer(_k: string, v) {
+  const date = v?.date;
+  if (date && date instanceof Date) {
+    return { ...v, date: date.toISOString().slice(0, 10) };
+  }
+  return v;
+}
+
 async function main() {
-  console.log(JSON.stringify(await loadTechnologyRadar(process.argv[2])));
+  console.log(
+    JSON.stringify(await loadTechnologyRadar(process.argv[2]), jsonReplacer),
+  );
 }
 
 main();
